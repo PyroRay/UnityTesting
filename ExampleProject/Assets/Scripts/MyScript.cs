ff25c2e6f44556b545678cdf33a7f1f04ada8378
Ray Peng <ygnihTtaC@users.noreply.github.com>
2020-08-02 11:17:49 -0700

    Added ExampleProject Folder

---
+++ ExampleProject/Assets/Scripts/MyScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    class Item
+    {
+        public string name = "Unnamed Item";
+        public string description = "Undescribed item.";
+        public int worth = 1;
+        public bool canBeSold = true;
+        public int weight = 0;
+
+        public string Name { get => name; set => name = value; }
+        public int Worth { get => worth; set => worth = value; }
+        public bool CanBeSold { get => canBeSold; set => canBeSold = value; }
+    }
+   
+    class Equipment : Item
+    {
+        public int currentDurability = 100;
+        public int maxDurability = 100;
+    }
+
+    enum ArmorType
+    {
+        Helmet,
+        Chest,
+        Gloves,
+        Girdle,
+        Boots
+    }
+
+    class Armor : Equipment
+    {
+        public ArmorType type = ArmorType.Helmet;
+        public int defense = 1;
+    }
+
+    enum WeaponType
+    {
+        Sword,
+        Axe,
+        Hammer,
+        Staff
+    }
+    class Weapon : Equipment
+    {
+        public WeaponType type = WeaponType.Sword;
+        public int minDamage = 1;
+        public int maxDamage = 2;
+        public float attackTime = .6f;
+        public bool dealsBluntDamage = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Item item = new Item();
+        item.Name = "Goblin Tooth";
+        item.Worth = 4;
+        item.CanBeSold = true;
+        Debug.Log("This " + item.Name + " is worth " + item.Worth + " golden coins!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ExampleProject/Assets/Scripts/SimpleRotation.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotation : MonoBehaviour
+{
+    [Header("Transform Vector")]
+    public Vector3 rotationPerSecond;
+    [Header("Time Multiplier")]
+    public float scalingFactor = 1;
+    private float p_scalingFactor;
+    private Vector3 p_rotationPerSecond = new Vector3();
+    private Vector3 c_rotationPerSecond = new Vector3();
+
+    void Start()
+    {
+        p_rotationPerSecond = rotationPerSecond;
+        p_scalingFactor = scalingFactor;
+    }
+
+        // Update is called once per frame
+    void Update()
+    {
+        //c_rotationPerSecond = rotationPerSecond;
+
+        if(rotationPerSecond != p_rotationPerSecond)
+        {
+            c_rotationPerSecond = rotationPerSecond;
+            p_rotationPerSecond = rotationPerSecond;
+        }
+
+        if(scalingFactor != p_scalingFactor)
+        {
+            //Debug.Log(string.Format("first check"));
+            if (p_scalingFactor != 0)
+            {
+                Debug.Log(string.Format("second check 1 {0}", p_scalingFactor));
+                c_rotationPerSecond.Scale(new Vector3((1 / p_scalingFactor), (1 / p_scalingFactor), (1 / p_scalingFactor)));
+            }
+            else
+            {
+                Debug.Log(string.Format("second check 2"));
+                c_rotationPerSecond = rotationPerSecond;
+            }
+            Debug.Log(string.Format("scalingFactor will be updated from {0} to {1}", p_scalingFactor, scalingFactor));
+            c_rotationPerSecond.Scale(new Vector3(scalingFactor, scalingFactor, scalingFactor));
+            //Debug.Log(string.Format("scalingFactor was updated from {0} to {1}", p_scalingFactor, scalingFactor));
+            p_scalingFactor = scalingFactor;
+        }
+        transform.Rotate(c_rotationPerSecond * Time.deltaTime);
+    }
+}
