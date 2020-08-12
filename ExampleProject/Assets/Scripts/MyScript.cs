373a51074f4ac0210b8f20fd992783a27cc9fdb8
Ray Peng <ygnihTtaC@users.noreply.github.com>
2020-08-12 09:45:37 -0700

    Added Many Things

--- ExampleProject/Assets/Scripts/MyScript.cs
+++ ExampleProject/Assets/Scripts/MyScript.cs
@@ -12,6 +12,15 @@ public class MyScript : MonoBehaviour
         public bool canBeSold = true;
         public int weight = 0;
 
+        public Item(string name, string description, int worth, bool canBeSold, int weight)
+        {
+            this.name = name;
+            this.description = description;
+            this.worth = worth;
+            this.canBeSold = canBeSold;
+            this.weight = weight;
+        }
+
         public string Name { get => name; set => name = value; }
         public int Worth { get => worth; set => worth = value; }
         public bool CanBeSold { get => canBeSold; set => canBeSold = value; }
@@ -21,6 +30,14 @@ public class MyScript : MonoBehaviour
     {
         public int currentDurability = 100;
         public int maxDurability = 100;
+
+        public Equipment(string name, string description, int worth, bool canBeSold, int weight, int maxDurability) : base(name, description, worth, canBeSold, weight)
+        {
+            //Apply max durability:
+            this.maxDurability = maxDurability;
+            //Make current durability match max durability:
+            currentDurability = maxDurability;
+        }
     }
 
     enum ArmorType
@@ -36,6 +53,12 @@ public class MyScript : MonoBehaviour
     {
         public ArmorType type = ArmorType.Helmet;
         public int defense = 1;
+
+        public Armor(string name, string description, int worth, bool canBeSold, int weight, int maxDurability, ArmorType type, int defense) : base(name, description, worth, canBeSold, weight, maxDurability)
+        {
+            this.type = type;
+            this.defense = defense;
+        }
     }
 
     enum WeaponType
@@ -52,16 +75,31 @@ public class MyScript : MonoBehaviour
         public int maxDamage = 2;
         public float attackTime = .6f;
         public bool dealsBluntDamage = false;
+
+        public Weapon(string name, string description, int worth, bool canBeSold, int weight, int maxDurability, WeaponType type, int minDamage, int maxDamage, float attackTime) : base(name, description, worth, canBeSold, weight, maxDurability)
+        {
+            this.type = type;
+            this.minDamage = minDamage;
+            this.maxDamage = maxDamage;
+            this.attackTime = attackTime;
+            //Set dealsBluntDamage based on weapon type:
+            if (type == WeaponType.Sword || type == WeaponType.Axe)
+                dealsBluntDamage = false;
+            else
+                dealsBluntDamage = true;
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        Item item = new Item();
-        item.Name = "Goblin Tooth";
-        item.Worth = 4;
-        item.CanBeSold = true;
-        Debug.Log("This " + item.Name + " is worth " + item.Worth + " golden coins!");
+        Item item = new Weapon("Rusty Axe", "A beat-up rusty axe.", 4, true, 8, 40, WeaponType.Axe, 4, 9, .6f);
+
+        Weapon weapon = (Weapon)item;
+
+        weapon = item as Weapon;
+
+        //Debug.Log("This " + item.Name + " is worth " + item.Worth + " golden coins!");
     }
 
     // Update is called once per frame
---
+++ LeGame/Assets/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveDown = KeyCode.S;
+    public KeyCode moveLeft = KeyCode.A;
+    public KeyCode moveRight = KeyCode.D;
+
+    public float speed = 10.0f;
+    public float jumpHeight = 10.0f;
+    private bool isJumping = false;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var vel = rb2d.velocity;
+        if (Input.GetKey(moveLeft))
+        {
+            vel.x = -speed;
+        }
+        else if (Input.GetKey(moveRight))
+        {
+            vel.x = speed;
+        }
+        else
+        {
+            vel.x = 0;
+        }
+
+        rb2d.velocity = vel;
+
+
+        if (Input.GetKeyDown(moveUp) && !isJumping) // both conditions can be in the same branch
+        {
+            Debug.Log("jump");
+            rb2d.AddForce(Vector2.up * jumpHeight); // you need a reference to the RigidBody2D component
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log(col.gameObject.tag);
+        if (col.gameObject.tag == "Platform") // GameObject is a type, gameObject is the property
+        {
+            isJumping = false;
+        }
+
+
+    }
+}
